8d5fdff7934a4cb6568263b3a18438d8f7aded4e
Mint <dhemfdldid@naver.com>
2023-07-20 17:29:00 +0900

    refactor : 독립 표현식 -> 변수에 JSX 컨텐츠 저장
    가장 깔끔한 방법. 변수에 JSX 컨텐츠 저장 후 출력

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -14,18 +14,24 @@ const Expenses = (props) =>  {
         return expense.date.getFullYear().toString() === filteredYear; //선택한 연도와 같은 경우 true 아닐경우 false 반환
     });
 
+    let expenseContext =  <p>No expense found.</p>;
+
+    if (filteredExpenses. length > 0) {
+        expenseContext = filteredExpenses.map((expense) => (
+            <ExpenseItem
+            key = {expense.id} //목록의 아이템을 매핑할 때는 반드시 key가 있어야 한다.
+            title = {expense.title}
+            amount = {expense.amount}
+            date = {expense.date}
+            />
+        ));
+    }
+
     return (
     <div>
         <Card className="expenses">
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {filteredExpenses.length === 0 && <p>No expense found</p>} {/*긴 조건부 표현을 &&연산자를 이용하여 두개의 독립 표현식으로 나눔*/}
-            {filteredExpenses.length > 0 && (filteredExpenses.map((expense) => <ExpenseItem
-                    key = {expense.id} //목록의 아이템을 매핑할 때는 반드시 key가 있어야 한다.
-                    title = {expense.title}
-                    amount = {expense.amount}
-                    date = {expense.date}
-                />))}
-
+            {expenseContext}
         </Card>
     </div>
     )
